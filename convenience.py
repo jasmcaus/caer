0a4eb84100b6eb89de8ef9eac576146230ddb355
jasmcaus <jasmcaus@gmail.com>
2020-08-13 13:34:43 +0530

    Modified convenience.py

--- convenience.py
+++ convenience.py
@@ -1,7 +1,17 @@
+# Author: Jason Dsouza
+# Github: http://www.github.com/jasmcaus
+
 # Importing the necessary packages
 import cv2 as cv
 import os
 import numpy as np
+# For Python 2.7
+if sys.version_info.major == 2:
+    from urllib import urlopen
+
+# For Python
+elif sys.version_info.major == 3:
+    from urllib.request import urlopen
 
 def readToGray(image,size):
     try:
@@ -58,3 +68,103 @@ def plotAcc(histories):
 
     plt.show()
 
+# -------------------- OPENCV IMAGE-SPECIFIC METHODS --------------------- 
+
+def translate(image, x, y):
+    # Defines the translation matrix and performs the translation
+    matrix = np.float32([[1, 0, x], [0, 1, y]])
+    shifted = cv2.warpAffine(image, matrix, (image.shape[1], image.shape[0]))
+
+    return shifted
+
+def rotate(image, angle, centre=None, scale=1.0):
+    # Grabs the dimensions of the image
+    (height, width) = image.shape[:2]
+
+    # If no centre is specified, we grab the centre coordinates of the image
+    if centre is None:
+        centre = (width // 2, height // 2)
+
+    # Rotates the image
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w, h))
+
+    return rotated
+
+def rotate_bound(image, angle):
+    # Grabs the dimensions of the image and then determines the centre
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w / 2, h / 2)
+
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+
+    # compute the new bounding dimensions of the image
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+
+    # Adjusts the rotation matrix to take into account translation
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+
+    # Performs the actual rotation and returns the image
+    return cv2.warpAffine(image, M, (nW, nH))
+
+def resize(image, width=None, height=None, interpolation=cv2.INTER_AREA):
+    """
+    Resizes the image while maintaing the aspect ratio of the original image
+    """
+    # Grabs the image dimensions 
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, we return the original image
+    if width is None and height is None:
+        return image
+
+    # If  width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # If height is None
+    else:
+        # Calculates the ratio of the width and constructs the dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # Resizes the image
+    resized = cv2.resize(image, dim, interpolation=interpolation)
+
+    return resized
+
+def toMatplotlib(image):
+    """
+    Converts BGR image ordering to RGB ordering
+    """
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def url_to_image(url, flag=cv2.IMREAD_COLOR):
+    # Converts the image to a Numpy array and reads it in OpenCV
+    response = urlopen(url)
+    image = np.asarray(bytearray(response.read()), dtype="uint8")
+    image = cv2.imdecode(image, flag)
+
+    return image
+
+def canny(image, sigma=0.33):
+    # computes the median of the single channel pixel intensities
+    med = np.median(image)
+
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * med))
+    upper = int(min(255, (1.0 + sigma) * med))
+    edged = cv2.Canny(image, lower, upper)
+
+    return edged
