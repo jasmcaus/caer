e5c3cd7af813e5f900ac42ff60d0be014e8e8b99
jasmcaus <jasmcaus@gmail.com>
2020-08-13 22:31:00 +0530

    Added setup.cfg; modified setup.py; added 'caer' to PyPi

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="caer",
+    version="0.0.1",
+    author="Jason Dsouza",
+    author_email="jasmcaus@gmail.com",
+    description="A Computer Vision library in Python with powerful image processing operations, including support for Deep Learning models built using the Keras framework",
+    url="https://github.com/jasmcaus/caer",
+    packages=setuptools.find_packages(),
+    keywords=['computer vision', 'deep learning', 'image processing', 'opencv', 'matplotlib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
