4b79e8120a5d9cc6498ade9d6732c9220a43e751
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-08-15 13:56:41 +0530

    Released v1.0.0 -- added tensorflow warning surpression

--- caer/__init__.py
+++ caer/__init__.py
@@ -1,24 +1,24 @@
 # Author: Jason Dsouza
 # Github: http://www.github.com/jasmcaus
 
-from .utils import preprocess
-from .utils import imageDataGenerator
-from .utils import sepTrain
-from .utils import normalize
+from .preprocess import preprocess
+from .preprocess import imageDataGenerator
+from .preprocess import sepTrain
+from .preprocess import normalize
 
 from .model import createDefaultModel
 from .model import LeNet
 from .model import saveModel
 from .model import testModel
 
-from .convenience import readToGray
-from .convenience import saveNumpy
-from .convenience import train_val_split
-from .convenience import plotAcc 
-from .convenience import url_to_image 
-from .convenience import toMatplotlib 
-from .convenience import translate 
-from .convenience import rotate 
-from .convenience import rotate_bound 
-from .convenience import resize 
-from .convenience import canny 
+from .utils import readToGray
+from .utils import saveNumpy
+from .utils import train_val_split
+from .utils import plotAcc 
+from .utils import url_to_image 
+from .utils import toMatplotlib 
+from .utils import translate 
+from .utils import rotate 
+from .utils import rotate_bound 
+from .utils import resize 
+from .utils import canny 
--- setup.py
+++ setup.py
@@ -1,27 +1,37 @@
 import setuptools
 
-long_description = """
-A Computer Vision library in Python with powerful image processing operations, including support for Deep Learning models built using the Keras framework
+DESCRIPTION = """A Computer Vision library in Python with powerful image processing operations, including support for Deep Learning models built using the Keras framework"""
+
+LONG_DESCRIPTION = """A Computer Vision library in Python with powerful image processing operations, including support for Deep Learning models built using the Keras framework
 
 This repository is actively being maintained. If there are any issues, kindly open a thread in the 'Issues' pane on the official Github repository. 
 """
 
 setuptools.setup(
     name="caer",
-    version="0.0.5",
+    version="1.0.0",
     author="Jason Dsouza",
     author_email="jasmcaus@gmail.com",
-    description="A Computer Vision library in Python with powerful image processing operations, including support for Deep Learning models built using the Keras framework",
-    long_description=long_description,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     url="https://github.com/jasmcaus/caer",
     packages=setuptools.find_packages(),
     license='MIT',
-    install_requires=['tensorflow', 'numpy', 'opencv-contrib-python', 'os'],
+    install_requires=['numpy', 'opencv-contrib-python'],
     keywords=['computer vision', 'deep learning', 'image processing', 'opencv', 'matplotlib'],
     classifiers=[
         "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
 )
