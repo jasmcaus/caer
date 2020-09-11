b19c2a0740fb37cb4f536ed24d358220d336eea9
jasmcaus <jasmcaus@gmail.com>
2020-09-11 23:31:18 +0530

    Bug fixes

--- caer/training.py
+++ caer/training.py
@@ -12,12 +12,13 @@ def create_dataset(X, y, dataset_name):
     """
 
     if '.h5' not in dataset_name:
-        dataset_name = dataset_name + '.h5'
+        dataset_name += '.h5'
 
     h5f = h5py.File('dataset_name', 'w')
     h5f.create_dataset('features', data=X)
     h5f.create_dataset('labels', data=y)
     h5f.close()
+    
 
 def load_dataset(dataset_name):
     if '.h5' not in dataset_name:
