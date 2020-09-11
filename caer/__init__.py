f37569f5a0d59733a8b37a20ca0a656f77b153fa
jasmcaus <jasmcaus@gmail.com>
2020-09-12 00:33:44 +0530

    sepTrain() --> sep_train()

--- caer/__init__.py
+++ caer/__init__.py
@@ -2,7 +2,7 @@
 # Github: http://www.github.com/jasmcaus
 
 from .preprocess import preprocess_from_directory
-from .preprocess import sepTrain
+from .preprocess import sep_train
 from .preprocess import shuffle
 from .preprocess import reshape
 from .preprocess import normalize
