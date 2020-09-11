4709bbeb88dc0e6fac098f40591e36a4e4ff8448
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-09-11 22:58:44 +0530

    Created load_dataset()

--- caer/training.py
+++ caer/training.py
@@ -18,3 +18,14 @@ def create_dataset(X, y, dataset_name):
     h5f.create_dataset('features', data=X)
     h5f.create_dataset('labels', data=y)
     h5f.close()
+
+def load_dataset(dataset_name):
+    if '.h5' not in dataset_name:
+        dataset_name = dataset_name + '.h5'
+    
+    h5f = h5py.File('dataset_name', 'r')
+    X = h5f['features'][:] 
+    y = h5f['labels'][:]
+    h5f.close()
+
+    return X, y
