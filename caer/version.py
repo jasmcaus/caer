0ee4d26a528aa94922da404be2d7bd94c3995020
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-09-15 13:24:24 +0530

    preprocess_from_dir() cleanup + released v1.5.2

--- caer/version.py
+++ caer/version.py
@@ -3,8 +3,8 @@
 # Copyright 2020, Caer
 
 author = 'Jason Dsouza: http://www.github.com/jasmcaus'
-version = '1.5.0'
-full_version = '1.5.0'
+version = '1.5.1'
+full_version = '1.5.1'
 release = True
 if not release:
     version = full_version
