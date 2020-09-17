5670b5a784af05c75d486c43745567026f1bbe6f
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-09-17 20:15:30 +0530

    Added FPS count

---
+++ caer/video/frames_and_fps.py
@@ -0,0 +1,22 @@
+# Author: Jason Dsouza
+# Github: http://www.github.com/jasmcaus
+
+from .videostream import VideoStream
+
+def count_frames(video_path=None):
+    if video_path is None:
+        raise ValueError('[ERROR] Specify a valid video path')
+
+    stream = VideoStream(video_path)
+    frame_count = stream.count_frames()
+    stream.release()
+    return frame_count
+
+def get_fps(video_path=None):
+    if video_path is None:
+        raise ValueError('[ERROR] Specify a valid video path')
+
+    stream = VideoStream(video_path)
+    fps_count = stream.get_fps()
+    stream.release()
+    return fps_count
