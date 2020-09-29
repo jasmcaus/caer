32ca9482d8ae33c5b568d1455fcb8c91a1dbd258
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-09-29 11:11:15 +0530

    Released v1.7.4

--- caer/_version.py
+++ caer/_version.py
@@ -2,8 +2,8 @@
 # Copyright 2020, Caer
 
 author = 'Jason Dsouza: http://www.github.com/jasmcaus'
-version = '1.7.3'
-full_version = '1.7.3'
+version = '1.7.4'
+full_version = '1.7.4'
 release = True
 if not release:
     version = full_version 
