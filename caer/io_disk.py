0917b98d907fe0d7da95810e590ffd03e08577ee
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-10-04 15:43:03 +0530

    Changes to read_image()

--- caer/io_disk.py
+++ caer/io_disk.py
@@ -4,14 +4,14 @@
 import os
 import cv2 as cv
 
-def read_image(path):
+def read_image(image_path):
     """Reads an image located at `path` into an array.
     Arguments:
         path (str): Path to a valid image file in the filesystem.
     Returns:
         `numpy.ndarray` of size `(height, width, channels)`.
     """
-    if not os.path.exists(path):
-        raise ValueError('[ERROR] Specified path does not exist')
+    if not os.path.exists(image_path):
+        raise FileNotFoundError('[ERROR] The image file was not found')
     
-    return cv.imread(path)
+    return cv.imread(image_path)
