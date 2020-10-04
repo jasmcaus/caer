d1a5eb3155f379a12fce15e2c8addd1302cff1eb
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-10-04 23:17:49 +0530

    Added resize()

---
+++ caer/resize.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+from .opencv import _cv2_resize
+
+def resize(image, resize_factor, interpolation="area"):
+    """
+        Resizes an image with a specified resizing factor, this factor can also be
+        the target shape of the resized image specified as a tuple.
+        Tuple must be of a shape 2 (width, height)
+    """
+    interpolation_methods = {
+        "area": cv.INTER_AREA,
+        "nearest": cv.INTER_NEAREST,
+        "bicubic": cv.INTER_CUBIC,
+        "bilinear": cv.INTER_LINEAR
+    }
+    if interpolation not in interpolation_methods:
+        raise ValueError('[ERROR] Specify a valid interpolation type')
+
+    if type(resize_factor) is not tuple:
+        new_shape = (int(resize_factor * image.shape[0]), int(resize_factor * image.shape[1]))
+    else:
+        if len(resize_factor) == 2:
+            new_shape = resize_factor
+        else:
+            raise ValueError('[ERROR] Tuple Shape must be equal to 2 (width,height)')
+
+    width, height = new_shape[:2]
+    return _cv2_resize(image, width, height, interpolation=interpolation_methods[interpolation])
