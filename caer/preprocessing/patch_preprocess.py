9a87851ed1a24efc95e84f071a191fb7dccfbe7a
jasmcaus <jasmcaus@gmail.com>
2020-10-05 10:35:45 +0530

    Updated file credits

--- caer/preprocessing/patch_preprocess.py
+++ caer/preprocessing/patch_preprocess.py
@@ -1,5 +1,5 @@
-# Author: Jason Dsouza
-# Github: http://www.github.com/jasmcaus
+# Copyright (c) 2020 Jason Dsouza <jasmcaus@gmail.com>
+# Protected under the MIT License (see LICENSE)
 
 # Importing the necessary packages
 from ._patches import extract_patches_2d
