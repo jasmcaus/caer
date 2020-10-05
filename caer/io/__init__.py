0081d9082d0bb09133bd9c5351cde10f38a801b8
jasmcaus <jasmcaus@gmail.com>
2020-10-05 10:32:05 +0530

    Updated file credits

--- caer/io/__init__.py
+++ caer/io/__init__.py
@@ -1,5 +1,5 @@
-# Author: Jason Dsouza
-# Github: http://www.github.com/jasmcaus
+# Copyright (c) 2020 Jason Dsouza <jasmcaus@gmail.com>
+# Protected under the MIT License (see LICENSE)
 
 from .io import HDF5Dataset
 from .io import load_dataset
