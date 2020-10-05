10c804f4f9dbad73cb5ac034c593e9a172a66664
jasmcaus <jasmcaus@gmail.com>
2020-10-05 11:04:45 +0530

    Renamed file

--- caer/video/__init__.py
+++ caer/video/__init__.py
@@ -3,9 +3,9 @@
 
 from .extract_frames import extract_frames
 
-from .LiveVideoStream import LiveVideoStream
+from .livevideostream import LiveVideoStream
 
-from .VideoStream import VideoStream
+from .videostream import VideoStream
 
 from .frames_and_fps import count_frames
 from .frames_and_fps import get_fps
