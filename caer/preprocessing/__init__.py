f7e9b03d737344e5dd6c4fd9f9c95c20f42e4327
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-10-12 14:44:25 +0530

    Added __all__

--- caer/preprocessing/__init__.py
+++ caer/preprocessing/__init__.py
@@ -8,3 +8,10 @@ from .mean_subtraction import subtract_mean
 from .mean_subtraction import _check_mean_sub_values
 
 from .patch_preprocess import PatchPreprocess
+
+__all_preprocessing__ = (
+    'MeanProcess',
+    'compute_mean',
+    'compute_mean_from_dir',
+    'subtract_mean',
+)
