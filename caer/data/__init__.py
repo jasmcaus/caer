ed23a1c5ae6fb57353ed0c332e8572cda7330217
jasmcaus <jasmcaus@gmail.com>
2020-10-16 23:40:24 +0530

    Added functions for the image data

--- caer/data/__init__.py
+++ caer/data/__init__.py
@@ -5,4 +5,112 @@ from .._checks import _check_size
 from ..utils import load_img 
 
 def bird(target_size=None):
-    
+    return load_img('bird.jpg', target_size=target_size)
+
+
+def blue_siamese(target_size=None):
+    return load_img('blue_siamese.jpg', target_size=target_size)
+
+
+def camera(target_size=None):
+    return load_img('camera.jpg', target_size=target_size)
+
+
+def candles(target_size=None):
+    return load_img('candles.jpg', target_size=target_size)
+
+
+def coast(target_size=None):
+    return load_img('coast.jpg', target_size=target_size)
+
+
+def controller(target_size=None):
+    return load_img('controller.jpg', target_size=target_size)
+
+
+def cycle(target_size=None):
+    return load_img('cycle.jpg', target_size=target_size)
+
+
+def fish(target_size=None):
+    return load_img('fish.jpg', target_size=target_size)
+
+
+def gold_fish(target_size=None):
+    return load_img('gold_fish.jpg', target_size=target_size)
+
+
+def jellyfish(target_size=None):
+    return load_img('jellyfish.jpg', target_size=target_size)
+
+
+def keyboard(target_size=None):
+    return load_img('keyboard.jpg', target_size=target_size)
+
+
+def lantern(target_size=None):
+    return load_img('lantern.jpg', target_size=target_size)
+
+
+def laptop(target_size=None):
+    return load_img('laptop.jpg', target_size=target_size)
+
+
+def lighthouse(target_size=None):
+    return load_img('lighthouse.jpg', target_size=target_size)
+
+
+def lights(target_size=None):
+    return load_img('lights.jpg', target_size=target_size)
+
+
+def maple(target_size=None):
+    return load_img('maple.jpg', target_size=target_size)
+
+
+def mountain(target_size=None):
+    return load_img('mountain.jpg', target_size=target_size)
+
+
+def night_tent(target_size=None):
+    return load_img('night_tent.jpg', target_size=target_size)
+
+
+def night(target_size=None):
+    return load_img('night.jpg', target_size=target_size)
+
+
+def phone(target_size=None):
+    return load_img('phone.jpg', target_size=target_size)
+
+
+def red_fish(target_size=None):
+    return load_img('red_fish.jpg', target_size=target_size)
+
+
+def sea(target_size=None):
+    return load_img('sea.jpg', target_size=target_size)
+
+
+def snow(target_size=None):
+    return load_img('snow.jpg', target_size=target_size)
+
+
+def squirrel(target_size=None):
+    return load_img('snow.jpg', target_size=target_size)
+
+
+def statue(target_size=None):
+    return load_img('statue.jpg', target_size=target_size)
+
+
+def tent(target_size=None):
+    return load_img('tent.jpg', target_size=target_size)
+
+
+def venice(target_size=None):
+    return load_img('venice.jpg', target_size=target_size)
+
+
+def whale(target_size=None):
+    return load_img('whale.jpg', target_size=target_size)
