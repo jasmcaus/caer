50c3f316dd9d7ab1094e8ad38718830690c83939
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-10-17 13:09:31 +0530

    load_img() moved to images.py

--- caer/images.py
+++ caer/images.py
@@ -2,8 +2,47 @@
 # Protected under the MIT License (see LICENSE)
 
 import math 
+import os 
 import cv2 as cv
+
 from ._checks import _check_size
+from .io_disk import _read_image
+from .opencv import to_rgb
+
+def load_img(image_path, target_size=None, channels=3, rgb=True):
+    """
+        Loads in an image from `image_path`
+        Arguments
+            image_path: Filepath to read the image from
+            target_size: Target image size
+            channels: 1 (grayscale) or 3 (RGB/BGR). Default: 1
+            rgb: Boolean to keep RGB ordering. Default: True
+    """
+    if not os.path.exists(image_path):
+        raise ValueError('Specified filepath does not exist')
+    
+    if target_size is not None:
+        _ = _check_size(target_size)
+        
+    if not isinstance(channels, int) or channels not in [1,3]:
+        raise ValueError('channels must be an integer - 1 (Grayscale) or 3 (RGB)')
+
+    if not isinstance(rgb, bool):
+        raise ValueError('rgb must be a boolean')
+
+    image_array = _read_image(image_path)
+
+    # [INFO] Using the following piece of code results in a 'None' in the training set
+    # if image_array == None:
+    #     pass
+    if channels == 1:
+        image_array = cv.cvtColor(image_array, cv.COLOR_BGR2GRAY)
+    if target_size is not None and _check_size(target_size):
+        image_array = cv.resize(image_array, target_size)
+    if rgb:
+        image_array = to_rgb(image_array)
+
+    return image_array
 
 
 def resize(image, target_size=None, resize_factor=None, keep_aspect_ratio=False, interpolation='area'):
