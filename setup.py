a72e3cd77e67781563bcdd955b3e00e895677157
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-10-17 14:37:12 +0530

    update comment

--- setup.py
+++ setup.py
@@ -133,8 +133,8 @@ def setup_package():
         python_requires=PYTHON_REQUIRES,
         keywords=KEYWORDS,
         classifiers= CLASSIFIERS,
-# Include_package_data is required for setup.py to recognize the MAINFEST.in
-#   https://python-packaging.readthedocs.io/en/latest/non-code-files.html
+# Include_package_data is required for setup.py to recognize the MAINFEST.in file
+# https://python-packaging.readthedocs.io/en/latest/non-code-files.html
         include_package_data=True
     )
 
