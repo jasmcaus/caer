56e2f632beca040d4b90151834fffedf1fb2aa35
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-10-17 12:54:11 +0530

    Added utils

---
+++ caer/utils/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2020 Jason Dsouza <jasmcaus@gmail.com>
+# Protected under the MIT License (see LICENSE)
+
+from .validators import URLValidator
+from .validators import validate_ipv6_address
