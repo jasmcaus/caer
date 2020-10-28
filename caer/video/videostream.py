fb09aa6d160011103ade40d90d171a7b3a6e3f05
jasmcaus <jasmcaus@gmail.com>
2020-10-28 22:38:49 +0530

    Update comment

--- caer/video/videostream.py
+++ caer/video/videostream.py
@@ -1,5 +1,19 @@
-# Copyright (c) 2020 Jason Dsouza <jasmcaus@gmail.com>
-# Protected under the MIT License (see LICENSE)
+# Copyright 2020 The Caer Authors. All Rights Reserved.
+# Copyright 2020 The Caer Authors. All Rights Reserved.
+#
+# Licensed under the MIT License (see LICENSE);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 
 from .filevideostream import FileVideoStream
 
