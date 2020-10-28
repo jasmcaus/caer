b7ab7558d11e14fcea09323a67f1af0c294aafa5
jasmcaus <jasmcaus@gmail.com>
2020-10-28 22:49:56 +0530

    Update comment

--- caer/paths.py
+++ caer/paths.py
@@ -1,5 +1,10 @@
-# Copyright (c) 2020 Jason Dsouza <jasmcaus@gmail.com>
-# Protected under the MIT License (see LICENSE)
+# Copyright 2020 The Caer Authors. All Rights Reserved.
+#
+# Licensed under the MIT License (see LICENSE);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at <https://opensource.org/licenses/MIT>
+#
+# ==============================================================================
 
 #pylint:disable=redefined-outer-name
 
