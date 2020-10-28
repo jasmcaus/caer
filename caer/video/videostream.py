664b388cdbf4a2052abcf7685f794a41605b306e
jasmcaus <jasmcaus@gmail.com>
2020-10-28 22:47:21 +0530

    Update comment

--- caer/video/videostream.py
+++ caer/video/videostream.py
@@ -2,15 +2,8 @@
 #
 # Licensed under the MIT License (see LICENSE);
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# You may obtain a copy of the License at <https://opensource.org/licenses/MIT>
 #
-#     https://opensource.org/licenses/MIT
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 # ==============================================================================
 
 
