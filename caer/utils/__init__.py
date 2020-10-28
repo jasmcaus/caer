b99f159cadc44cc28b29cd1c0955a4a0daa5007b
jasmcaus <jasmcaus@gmail.com>
2020-10-28 15:13:45 +0530

    Removed timezone-specific methods

--- caer/utils/__init__.py
+++ caer/utils/__init__.py
@@ -5,14 +5,9 @@ from .validators import URLValidator
 from .validators import validate_ipv6_address
 from .validators import is_valid_url
 
-from .timezone import get_fixed_timezone
-from .timezone import now
-
 
 __all__ = (
     'URLValidator',
     'validate_ipv6_address',
     'is_valid_url'
-    'get_fixed_timezone',
-    'now',
 )
