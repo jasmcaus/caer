c9fcc82ada69735142109537a6c60042760590f8
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-10-31 01:54:29 +0530

    Moved __all__

--- caer/opencv.py
+++ caer/opencv.py
@@ -11,19 +11,6 @@ import numpy as np
 from urllib.request import urlopen
 
 
-__all__ = [
-    'get_opencv_version',
-    'to_gray',
-    'to_hsv',
-    'to_lab',
-    'to_rgb',
-    'url_to_image',
-    'translate',
-    'rotate',
-    'edges'
-]
-
-
 def get_opencv_version():
     return cv.__version__[0]
 
@@ -161,3 +148,16 @@ def url_to_image(url, rgb=True):
     if rgb:
         image = to_rgb(image)
     return image
+
+
+__all__ = [
+    'get_opencv_version',
+    'to_gray',
+    'to_hsv',
+    'to_lab',
+    'to_rgb',
+    'url_to_image',
+    'translate',
+    'rotate',
+    'edges'
+]
