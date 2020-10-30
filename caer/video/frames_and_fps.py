74612471204bcf2559998c8449bd56b3cc069624
jasmcaus <jasmcaus@gmail.com>
2020-10-31 01:52:56 +0530

    Moved __all__

--- caer/video/frames_and_fps.py
+++ caer/video/frames_and_fps.py
@@ -9,12 +9,6 @@
 from .videostream import VideoStream
 
 
-__all__ = [
-    'count_frames',
-    'get_fps'
-]
-
-
 def count_frames(video_path=None):
     if video_path is None:
         raise ValueError('Specify a valid video path')
@@ -33,3 +27,9 @@ def get_fps(video_path=None):
     fps_count = stream.get_fps()
     stream.release()
     return fps_count
+
+
+__all__ = [
+    'count_frames',
+    'get_fps'
+]
