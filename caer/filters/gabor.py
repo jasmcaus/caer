70247dcd3be18ebaab67c915711c6a111b4918df
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-07 22:17:42 +0530

    Add comment

---
+++ caer/filters/gabor.py
@@ -0,0 +1,7 @@
+# Copyright 2020 The Caer Authors. All Rights Reserved.
+#
+# Licensed under the MIT License (see LICENSE);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at <https://opensource.org/licenses/MIT>
+#
+# ==============================================================================
