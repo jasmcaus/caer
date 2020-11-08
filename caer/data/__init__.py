c19875f18baf44b9d0bab22e764390939935e50a
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-08 23:21:23 +0530

    Changed to imread

--- caer/data/__init__.py
+++ caer/data/__init__.py
@@ -123,7 +123,7 @@ def sunrise(target_size=None, rgb=True):
 
 
 def tent(target_size=None, rgb=True):
-    return load_img(here+'tent.jpg', target_size=target_size, rgb=rgb)
+    return imread(here+'tent.jpg', target_size=target_size, rgb=rgb)
 
 
 __all__ = [d for d in dir() if not d.startswith('_')]
