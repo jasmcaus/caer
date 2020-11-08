2800173fe63cbbe3760372b97a420e82b658471d
jasmcaus <jasmcaus@gmail.com>
2020-11-08 13:43:53 +0530

    Add gabor kernel

--- caer/filters/gabor.py
+++ caer/filters/gabor.py
@@ -5,3 +5,68 @@
 # You may obtain a copy of the License at <https://opensource.org/licenses/MIT>
 #
 # ==============================================================================
+
+import numpy as np 
+
+def _sigma_prefactor(bandwidth):
+    b = bandwidth
+    # See http://www.cs.rug.nl/~imaging/simplecell.html
+    return 1.0 / np.pi * np.sqrt(np.log(2) / 2.0) * \
+        (2.0 ** b + 1) / (2.0 ** b - 1)
+
+
+def gabor_kernel(frequency, theta=0, bandwidth=1, sigma_x=None, sigma_y=None,
+                 n_stds=3, offset=0):
+    """Return complex 2D Gabor filter kernel.
+    Gabor kernel is a Gaussian kernel modulated by a complex harmonic function.
+    Harmonic function consists of an imaginary sine function and a real
+    cosine function. Spatial frequency is inversely proportional to the
+    wavelength of the harmonic and to the standard deviation of a Gaussian
+    kernel. The bandwidth is also inversely proportional to the standard
+    deviation.
+    Parameters
+    ----------
+    frequency : float
+        Spatial frequency of the harmonic function. Specified in pixels.
+    theta : float, optional
+        Orientation in radians. If 0, the harmonic is in the x-direction.
+    bandwidth : float, optional
+        The bandwidth captured by the filter. For fixed bandwidth, ``sigma_x``
+        and ``sigma_y`` will decrease with increasing frequency. This value is
+        ignored if ``sigma_x`` and ``sigma_y`` are set by the user.
+    sigma_x, sigma_y : float, optional
+        Standard deviation in x- and y-directions. These directions apply to
+        the kernel *before* rotation. If `theta = pi/2`, then the kernel is
+        rotated 90 degrees so that ``sigma_x`` controls the *vertical*
+        direction.
+    n_stds : scalar, optional
+        The linear size of the kernel is n_stds (3 by default) standard
+        deviations
+    offset : float, optional
+        Phase offset of harmonic function in radians.
+    Returns
+    -------
+    g : complex array
+        Complex filter kernel.
+    """
+
+    if sigma_x is None:
+        sigma_x = _sigma_prefactor(bandwidth) / frequency
+    if sigma_y is None:
+        sigma_y = _sigma_prefactor(bandwidth) / frequency
+
+    x0 = np.ceil(max(np.abs(n_stds * sigma_x * np.cos(theta)),
+                     np.abs(n_stds * sigma_y * np.sin(theta)), 1))
+    y0 = np.ceil(max(np.abs(n_stds * sigma_y * np.cos(theta)),
+                     np.abs(n_stds * sigma_x * np.sin(theta)), 1))
+    y, x = np.mgrid[-y0:y0 + 1, -x0:x0 + 1]
+
+    rotx = x * np.cos(theta) + y * np.sin(theta)
+    roty = -x * np.sin(theta) + y * np.cos(theta)
+
+    g = np.zeros(y.shape, dtype=np.complex)
+    g[:] = np.exp(-0.5 * (rotx ** 2 / sigma_x ** 2 + roty ** 2 / sigma_y ** 2))
+    g /= 2 * np.pi * sigma_x * sigma_y
+    g *= np.exp(1j * (2 * np.pi * frequency * rotx + offset))
+
+    return g
