0c228138e1bca163534774999aeae38a078cebd2
jasmcaus <jasmcaus@gmail.com>
2020-11-09 10:51:59 +0530

    Add comment

--- caer/numps/dispath.hpp
+++ caer/numps/dispath.hpp
@@ -1,7 +1,11 @@
-/* Copyright 2010-2014 (C)
- * Luis Pedro Coelho <luis@luispedro.org>
- * License: MIT
- */
+// Copyright 2020 The Caer Authors. All Rights Reserved.
+//
+// Licensed under the MIT License (see LICENSE);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at <https://opensource.org/licenses/MIT>
+//
+// ==============================================================================
+
 typedef unsigned char uchar;
 typedef unsigned short ushort;
 #define HANDLE_INTEGER_TYPES() \
