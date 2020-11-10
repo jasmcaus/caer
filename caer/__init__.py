f6f8cadb212e1ce6d4b523a6bfc6c3e01023446e
jasmcaus <jasmcaus@gmail.com>
2020-11-11 01:03:48 +0530

    Add edges

--- caer/__init__.py
+++ caer/__init__.py
@@ -161,6 +161,7 @@ from .visualizations import __all__ as __all_visualizations__
 from .distance import __all__ as __all_distance__
 from .convolve import __all__ as __all_convolve__
 from .morph import __all__ as __all_morph__
+from .edges import __all__ as __all_edges__ 
 
 from .video import __all__ as __all_video__
 from .preprocessing import __all__ as __all_preprocessing__
@@ -170,7 +171,7 @@ from .path import __all__ as __all_path__
 from .filters import __all__ as __all_filters__
 
 
-__all__ = __all_configs__ + __all_io__ + __all_resize__ + __all_opencv__ + __all_preprocess__ + __all_time__ + __all_utilities__ + __all_visualizations__ + __all_distance__ + __all_convolve__ + __all_morph__ + __all_filters__
+__all__ = __all_configs__ + __all_io__ + __all_resize__ + __all_opencv__ + __all_preprocess__ + __all_time__ + __all_utilities__ + __all_visualizations__ + __all_distance__ + __all_convolve__ + __all_morph__ + __all_filters__ + __all_edges__
 
 __all__ += __all_preprocessing__ 
 __all__ += __all_video__ 
