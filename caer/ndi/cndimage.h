ae9e859b3d449cb570c8686107ed4b0d2db96b3b
jasmcaus <jasmcaus@gmail.com>
2020-11-11 21:58:45 +0530

    Add cndimage

---
+++ caer/ndi/cndimage.h
@@ -0,0 +1,11 @@
+
+
+#ifndef ND_IMAGE_H
+#define ND_IMAGE_H
+
+#include "Python.h"
+
+#define PY_ARRAY_UNIQUE_SYMBOL _scipy_ndimage_ARRAY_API
+#include <numpy/arrayobject.h>
+
+#endif /* ND_IMAGE_H */
