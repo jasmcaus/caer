1e3fa330279f0257975f2dcd8602fde3e9835b72
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-12 10:20:18 +0530

    Moved __all__

--- caer/preprocessing/patch_preprocess.py
+++ caer/preprocessing/patch_preprocess.py
@@ -9,6 +9,11 @@
 from ._patches import extract_patches_2d
 
 
+__all__ = [
+    'PatchPreprocess'
+]
+
+
 class PatchPreprocess:
     def __init__(self, width, height):
         self.width = width
@@ -16,8 +21,3 @@ class PatchPreprocess:
 
     def patch_preprocess(self, image):
         return extract_patches_2d(image, (self.height, self.width), max_patches=1)[0]
-
-
-__all__ = [
-    'PatchPreprocess'
-]
