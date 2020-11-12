e6b69bf3896ea3d51254804b370ceda9b096feb4
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-12 12:44:00 +0530

    Update comment

--- caer/ndi/fourier.py
+++ caer/ndi/fourier.py
@@ -1,3 +1,12 @@
+#
+#  _____ _____ _____ _____
+# |     |     | ___  | __|  Caer - Modern Computer Vision
+# |     | ___ |      | \    Languages: Python, C, C++
+# |_____|     | ____ |  \   http://github.com/jasmcaus/caer
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
 
 
 import numpy as np
