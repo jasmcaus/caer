7e97b3670050978580046c39566ff797026ce27e
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-12 13:04:29 +0530

    Restricted file from being called directly

--- caer/_base.py
+++ caer/_base.py
@@ -8,9 +8,11 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
 
+
 # We don't want to modify the root dir in any way 
 # This gets distrupted if this file is called directly
 # Checks are in place:
+
 if __name__ != '__main__':
     from .path import dirname
 
