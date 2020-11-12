47b20442abde72c783faa385634695ed551b9d95
jasmcaus <jasmcaus@gmail.com>
2020-11-12 12:47:48 +0530

    Update comment

--- caer/_sklearn_utils.py
+++ caer/_sklearn_utils.py
@@ -1,14 +1,3 @@
-#
-#  _____ _____ _____ ____
-# |     | ___ | ___  | __|  Caer - Modern Computer Vision
-# |     |     |      | \    version 3.9.1
-# |_____|     | ____ |  \   http://github.com/jasmcaus/caer
-
-# Licensed under the MIT License <http://opensource.org/licenses/MIT>.
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>.
-# 
-
 #pylint: disable=bare-except,protected-access,c-extension-no-member
 
 import numbers
