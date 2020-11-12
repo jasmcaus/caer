54b0ac047434d6469c329c09a2879edda42ae94f
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-12 11:22:25 +0530

    Update comment

--- caer/utils/exceptions.py
+++ caer/utils/exceptions.py
@@ -1,3 +1,14 @@
+#
+#  _____ _____ _____ ____
+# |     | ___ | ___  | __|  Caer - Modern Computer Vision
+# |     |     |      | \    version 3.9.1
+# |_____|     | ____ |  \   http://github.com/jasmcaus/caer
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>.
+# 
+
 """
 Global Caer exception and warning classes.
 """
