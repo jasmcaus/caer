7bf9146de489341c6c1e016b0b696c2d9a55f22a
jasmcaus <jasmcaus@gmail.com>
2020-11-12 12:36:23 +0530

    Modify setup

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ MINOR = 8
 MICRO = 0
 ISRELEASED = True
 VERSION = f'{MAJOR}.{MINOR}.{MICRO}'
-PREV_VERSION = tuple(list)
 
 RUN_CYTHON_BUILD = True
 
@@ -192,26 +191,6 @@ def get_docs_url():
     #     # to the associated docs easily.
     #     return "https://caer.com/doc/{}.{}".format(MAJOR, MINOR)
     return URL + '/blob/master/docs/README.md'
-
-
-def replace_version(old_version, new_version):
-    import os 
-    if not isinstance(old_version, tuple) or not isinstance(new_version, tuple):
-        raise ValueError('`old_version` and `new_version` must be a version tuple. Eg: (1.2.3)')
-
-    major, minor, micro = old_version[:3]
-    old_version = f'{major}.{minor}.{micro}'
-    major, minor, micro = new_version[:3]
-    new_version = f'{major}.{minor}.{micro}'
-
-    for root, _, files in os.walk('../caer'):
-        for file in files:
-            if file.endswith(('.py', '.cpp', '.c', '.h', '.hpp')):
-                with open(os.path.abspath(os.path.join(root, file)), 'r') as f:
-                    new_text = f.read().replace('version ' + old_version, 'version ' + new_version)
-
-                with open(os.path.abspath(os.path.join(root, file)), 'w') as f:
-                    f.write(new_text)
 
 
 CYTHON_SOURCES = ('',)
