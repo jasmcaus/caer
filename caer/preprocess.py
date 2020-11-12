24cc55217517f12aa04e5dabe0f3f817a25cee0f
jasmcaus <jasmcaus@gmail.com>
2020-11-12 13:18:04 +0530

    Moved __all__

--- caer/preprocess.py
+++ caer/preprocess.py
@@ -19,6 +19,15 @@ from ._internal import _check_target_size, _check_mean_sub_values
 from .path import listdir, minijoin, exists, list_images
 
 
+__all__ = [
+    'preprocess_from_dir',
+    'sep_train',
+    'shuffle',
+    'reshape',
+    'normalize'
+]
+
+
 def preprocess_from_dir(DIR, 
                         classes=None, 
                         IMG_SIZE=(224,224), 
@@ -260,12 +269,3 @@ def normalize(x, dtype='float32'):
     # Converting to float32 and normalizing (float32 saves memory)
     x = x.astype(dtype) / 255
     return x
-
-
-__all__ = [
-    'preprocess_from_dir',
-    'sep_train',
-    'shuffle',
-    'reshape',
-    'normalize'
-]
