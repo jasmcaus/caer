7f90ea54a1abba87c8f5de69361e7c7c7c7bdcf6
jasmcaus <jasmcaus@gmail.com>
2020-11-13 16:33:50 +0530

    Update comment

--- caer/cfilters.cpp
+++ caer/cfilters.cpp
@@ -1,13 +1,16 @@
 /*
- _____ _____ _____ _____
-|     |     | ___  | __|  Caer - Modern Computer Vision
-|     | ___ |      | \    Languages: Python, C, C++
-|_____|     | ____ |  \   http://github.com/jasmcaus/caer
+   _____           ______  _____ 
+ / ____/    /\    |  ____ |  __ \
+| |        /  \   | |__   | |__) | Caer - Modern Computer Vision
+| |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
+| |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
+ \_____\/_/    \_ \______ |_|__\_
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>
 SPDX-License-Identifier: MIT
 Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
 */
+
 
 #define NO_IMPORT_ARRAY
 
