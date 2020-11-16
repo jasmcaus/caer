63d41aab0f022b8b8c3e29d59325da07ddd7eb79
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-16 16:19:41 +0530

    Add credit where necessary

--- caer/ndi/includes/cndimage.c
+++ caer/ndi/includes/cndimage.c
@@ -1,14 +1,5 @@
 /*
-   _____           ______  _____ 
- / ____/    /\    |  ____ |  __ \
-| |        /  \   | |__   | |__) | Caer - Modern Computer Vision
-| |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
-| |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
- \_____\/_/    \_ \______ |_|__\_
-
-Licensed under the MIT License <http://opensource.org/licenses/MIT>
-SPDX-License-Identifier: MIT
-Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
+Pulled from the Scipy Github Repo (to speed up import speed for Caer)
 */
 
 
