b62cad4bad948cbda2f263aa9e1ebd2a32df6723
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-16 16:15:48 +0530

    Fixed formatting issues

--- caer/filters/filter.py
+++ caer/filters/filter.py
@@ -23,9 +23,11 @@ modes = frozenset(mode2int.keys())
 
 def _checked_mode2int(mode, cval, fname):
     if mode not in modes:
-        raise ValueError('caer.%s: `mode` not in %s' % (fname, modes))
+        raise ValueError(f'caer.{fname}: `mode` not in {modes}')
+
     if mode == 'constant' and cval != 0.:
         raise NotImplementedError('Please email caer developers to get this implemented.')
+    
     return mode2int[mode]
 
 _check_mode = _checked_mode2int
