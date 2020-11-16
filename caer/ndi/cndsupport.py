a0bdf6b593887d7fd10a650d9e0bc1851adb0d3a
jasmcaus <jasmcaus@gmail.com>
2020-11-16 16:21:29 +0530

    Fixed formatting issues

--- caer/ndi/cndsupport.py
+++ caer/ndi/cndsupport.py
@@ -37,19 +37,23 @@ def _normalize_sequence(inp, rank):
     check if its length is equal to the length of array.
     """
     is_str = isinstance(inp, str)
+
     if not is_str and isinstance(inp, Iterable):
         normalized = list(inp)
         if len(normalized) != rank:
             err = "sequence argument must have length equal to inp rank"
             raise RuntimeError(err)
+
     else:
         normalized = [inp] * rank
+        
     return normalized
 
 
 def _get_output(output, inp, shape=None, complex_output=False):
     if shape is None:
         shape = inp.shape
+
     if output is None:
         if not complex_output:
             output = np.zeros(shape, dtype=input.dtype.name)
@@ -68,6 +72,7 @@ def _get_output(output, inp, shape=None, complex_output=False):
         output = np.typeDict[output]
         if complex_output and np.dtype(output).kind != 'c':
             raise RuntimeError("output must have complex dtype")
+
         output = np.zeros(shape, dtype=output)
 
     elif output.shape != shape:
@@ -75,4 +80,5 @@ def _get_output(output, inp, shape=None, complex_output=False):
     
     elif complex_output and output.dtype.kind != 'c':
         raise RuntimeError("output must have complex dtype")
+
     return output
