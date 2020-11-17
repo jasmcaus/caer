5a1f14ff7d388b00d0d5a364e1773a2cc64c1fb3
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-17 09:37:27 +0530

    Release v1.8.2 (fixed import errors in v1.8.1)

--- caer/__init__.py
+++ caer/__init__.py
@@ -179,7 +179,7 @@ from .distance import __all__ as __all_distance__
 from .morph import __all__ as __all_morph__
 
 
-__all__ = __all_globals__ + __all_io__ + __all_resize__ + __all_opencv__ + __all_preprocess__ + __all_time__ + __all_utilities__ + __all_visualizations__  + __all_filters__ + __all_distance__ + __all_morph__
+__all__ = __all_globals__ + __all_io__ + __all_resize__ + __all_opencv__ + __all_preprocess__ + __all_time__ + __all_utilities__ + __all_visualizations__ + __all_distance__ + __all_morph__
 
 __all__ += __all_preprocessing__ 
 __all__ += __all_video__ 
@@ -202,5 +202,5 @@ del __all_video__
 del __all_data__ 
 del __all_utils__ 
 del __all_distance__
-del __all_filters__
+# del __all_filters__
 del __all_morph__
