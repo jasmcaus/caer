faca12d918afe3f87eed3c7fa062a3675a98afe7
jasmcaus <jasmcaus@gmail.com>
2020-11-18 16:12:15 +0530

    Update before install

--- tools/travis/before-install.sh
+++ tools/travis/before-install.sh
@@ -14,3 +14,23 @@ fi
 
 # For debugging:
 conda info -a
+
+# python -m pip install --upgrade pip wheel
+
+# # install specific wheels from wheelhouse
+# for requirement in opencv-contrib-python requests numpy; do
+#     WHEELS="$WHEELS $(grep $requirement requirements/default.txt)"
+# done
+
+# # cython is not in the default.txt requirements
+# WHEELS="$WHEELS $(grep -i cython requirements/build.txt)"
+# python -m pip install $PIP_FLAGS $WHEELHOUSE $WHEELS
+
+# # Install build time requirements
+# python -m pip install $PIP_FLAGS -r requirements/build.txt
+# # Default requirements are necessary to build because of lazy importing
+# # They can be moved after the build step if #3158 is accepted
+# python -m pip install $PIP_FLAGS -r requirements/default.txt
+
+# # Show what's installed
+# python -m pip list
