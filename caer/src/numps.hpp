1b4ebf1bbaface8900624b87e620fd276170fe9a
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-18 19:45:22 +0530

    Update comment

--- caer/src/numps.hpp
+++ caer/src/numps.hpp
@@ -4,7 +4,7 @@
 | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
 | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
 | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
- \_____\/_/    \_ \______ |_|__\_
+ \_____\/_/    \_ \______ |_|  \_
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>
 SPDX-License-Identifier: MIT
