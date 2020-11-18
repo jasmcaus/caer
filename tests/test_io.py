4dc733ef3b21c61890238e381fdfa9ac7b26cc8f
jasmcaus <jasmcaus@gmail.com>
2020-11-18 08:59:31 +0530

    Fixed bugs + Add new test

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,13 +1,24 @@
 import caer 
 import os 
+import cv2 as cv 
+import numpy as np 
 
 here = os.path.dirname(__file__)
 
+
+def test_imread():
+    test_img = os.path.join(here, 'caer', 'data', 'bear.jpg')
+
+    img = caer.imread(test_img)
+    test_against = cv.imread(test_img) 
+
+    assert np.all(img == test_against)
+
+    
 def test_gray():
-    test_img = os.path.join(here, 'data', 'blue_tang.png')
+    test_img = os.path.join(here, 'caer', 'data', 'blue_tang.jpg')
 
     img = caer.imread(test_img, channels=1)
 
     assert len(img.shape) == 2
-
 
