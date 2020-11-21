1341312ba3f6a3a485cf74bdb6e05d0df16bb425
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-21 20:36:58 +0530

    De-bloat codebase

--- caer/utils/__init__.py
+++ caer/utils/__init__.py
@@ -9,11 +9,6 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
 
-
-from .validators import URLValidator
-from .validators import validate_ipv6_address
-from .validators import is_valid_url
-
 from .dtype import (
     img_as_float32, 
     img_as_float64, 
@@ -26,17 +21,9 @@ from .dtype import (
     dtype_limits
 )
 
-from .warnings import (
-    all_warnings, 
-    expected_warnings, 
-    warn
-)
-
 # __all__ globals 
-from .validators import __all__ as __all_validators__ 
 from .dtype import __all__ as __all_dtype__
 
-__all__ = __all_validators__ + __all_dtype__
+__all__ = __all_dtype__
 
 del __all_dtype__
-del __all_validators__
