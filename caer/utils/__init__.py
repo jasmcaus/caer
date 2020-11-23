e4ffeba32bad702664fdb68ba16342a3455cb38f
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-23 10:57:53 +0530

    Changed comment

--- caer/utils/__init__.py
+++ caer/utils/__init__.py
@@ -3,11 +3,12 @@
 # | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
 # | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
 # | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
-#  \_____\/_/    \_ \______ |_|  \_
+#  \_____\/_/    \_ \______ |_|  \_\
 
 # Licensed under the MIT License <http://opensource.org/licenses/MIT>
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
+
 
 from .dtype import (
     img_as_float32, 
