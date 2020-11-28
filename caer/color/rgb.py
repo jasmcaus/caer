40db76e5d98d66af42a9d3b94ffa87b58c5d0d11
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-28 20:51:50 +0530

    Update rgb

---
+++ caer/color/rgb.py
@@ -0,0 +1,53 @@
+#    _____           ______  _____ 
+#  / ____/    /\    |  ____ |  __ \
+# | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
+# | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
+# | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
+#  \_____\/_/    \_ \______ |_|  \_\
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
+
+import cv2 as cv 
+from .constants import RGB2BGR, RGB2GRAY, RGB2HSV, RGB2LAB
+
+
+def rgb_to_bgr(img):
+    """
+        Converts an RGB image to its BGR version
+    """
+    if len(img.shape) != 3:
+        raise ValueError(f'Image of shape 3 expected. Found shape {len(img.shape)}. This method converts an RGB image to its BGR counterpart')
+
+    return cv.cvtColor(img, RGB2BGR)
+
+
+def rgb_to_gray(img):
+    """
+        Converts an RGB image to its Grayscale version
+    """
+    if len(img.shape) != 3:
+        raise ValueError(f'Image of shape 3 expected. Found shape {len(img.shape)}. This method converts an RGB image to its Grayscale counterpart')
+    
+    return cv.cvtColor(img, RGB2GRAY)
+
+
+def rgb_to_hsv(img):
+    """
+        Converts an RGB image to its HSV counterpart
+    """
+    if len(img.shape) != 3:
+        raise ValueError(f'Image of shape 3 expected. Found shape {len(img.shape)}. This method converts an RGB image to its HSV counterpart')
+    
+    return cv.cvtColor(img, RGB2HSV)
+
+
+def rgb_to_lab(img):
+    """
+        Converts an RGB image to its LAB counterpart
+    """
+    if len(img.shape) != 3:
+        raise ValueError(f'Image of shape 3 expected. Found shape {len(img.shape)}. This method converts an RGB image to its LAB counterpart')
+
+    return cv.cvtColor(img, RGB2LAB)
