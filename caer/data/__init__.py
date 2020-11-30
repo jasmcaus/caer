aaca986aa64c21da545b952a974cb1fea8a2289c
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-30 22:18:34 +0530

    Add typing support to data

--- caer/data/__init__.py
+++ caer/data/__init__.py
@@ -12,122 +12,124 @@
 from ..io import imread 
 from ..path import abspath, minijoin
 from .._base import __curr__ 
+import numpy as np 
+
 
 here = minijoin(__curr__, 'data').replace('\\', "/") + "/"
 
-def get_path_to_data(name):
+def get_path_to_data(name) -> str:
     return minijoin(here, name)
 
 
-def audio_mixer(target_size=None, rgb=False):
+def audio_mixer(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'audio_mixer.jpg', target_size=target_size, rgb=rgb)
 
 
-def bear(target_size=None, rgb=False):
+def bear(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'bear.jpg', target_size=target_size, rgb=rgb)
 
 
-def beverages(target_size=None, rgb=False):
+def beverages(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'beverages.jpg', target_size=target_size, rgb=rgb)
 
 
-def black_cat(target_size=None, rgb=False):
+def black_cat(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'black_cat.jpg', target_size=target_size, rgb=rgb)
 
 
-def blue_tang(target_size=None, rgb=False):
+def blue_tang(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'blue_tang.jpg', target_size=target_size, rgb=rgb)
 
 
-def camera(target_size=None, rgb=False):
+def camera(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'camera.jpg', target_size=target_size, rgb=rgb)
 
 
-def controller(target_size=None, rgb=False):
+def controller(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'controller.jpg', target_size=target_size, rgb=rgb)
 
 
-def drone(target_size=None, rgb=False):
+def drone(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'drone.jpg', target_size=target_size, rgb=rgb)
 
 
-def dusk(target_size=None, rgb=False):
+def dusk(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'dusk.jpg', target_size=target_size, rgb=rgb)
 
 
-def fighter_fish(target_size=None, rgb=False):
+def fighter_fish(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'fighter_fish.jpg', target_size=target_size, rgb=rgb)
 
 
-def gold_fish(target_size=None, rgb=False):
+def gold_fish(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'gold_fish.jpg', target_size=target_size, rgb=rgb)
 
 
-def green_controller(target_size=None, rgb=False):
+def green_controller(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'green_controller.jpg', target_size=target_size, rgb=rgb)
 
 
-def green_fish(target_size=None, rgb=False):
+def green_fish(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'green_fish.jpg', target_size=target_size, rgb=rgb)
 
 
-def guitar(target_size=None, rgb=False):
+def guitar(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'guitar.jpg', target_size=target_size, rgb=rgb)
 
 
-def island(target_size=None, rgb=False):
+def island(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'island.jpg', target_size=target_size, rgb=rgb)
 
 
-def jellyfish(target_size=None, rgb=False):
+def jellyfish(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'jellyfish.jpg', target_size=target_size, rgb=rgb)
 
 
-def laptop(target_size=None, rgb=False):
+def laptop(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'laptop.jpg', target_size=target_size, rgb=rgb)
 
 
-def mountain(target_size=None, rgb=False):
+def mountain(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'mountain.jpg', target_size=target_size, rgb=rgb)
 
 
-def night(target_size=None, rgb=False):
+def night(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'night.jpg', target_size=target_size, rgb=rgb)
 
 
-def puppies(target_size=None, rgb=False):
+def puppies(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'puppies.jpg', target_size=target_size, rgb=rgb)
 
 
-def puppy(target_size=None, rgb=False):
+def puppy(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'puppy.jpg', target_size=target_size, rgb=rgb)
 
 
-def red_fish(target_size=None, rgb=False):
+def red_fish(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'red_fish.jpg', target_size=target_size, rgb=rgb)
 
 
-def phone(target_size=None, rgb=False):
+def phone(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'rotary_phone.jpg', target_size=target_size, rgb=rgb)
 
 
-def sea_turtle(target_size=None, rgb=False):
+def sea_turtle(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'sea_turtle.jpg', target_size=target_size, rgb=rgb)
 
 
-def snow(target_size=None, rgb=False):
+def snow(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'snow.jpg', target_size=target_size, rgb=rgb)
 
 
-def snowflake(target_size=None, rgb=False):
+def snowflake(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'snowflake.jpg', target_size=target_size, rgb=rgb)
 
 
-def sunrise(target_size=None, rgb=False):
+def sunrise(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'sunrise.jpg', target_size=target_size, rgb=rgb)
 
 
-def tent(target_size=None, rgb=False):
+def tent(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'tent.jpg', target_size=target_size, rgb=rgb)
 
 
