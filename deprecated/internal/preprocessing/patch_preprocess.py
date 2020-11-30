bc9ae8452ad38035a2fc83986ec460e2f0ba55aa
jasmcaus <jasmcaus@gmail.com>
2020-11-30 22:11:28 +0530

    Updates

---
+++ deprecated/internal/preprocessing/patch_preprocess.py
@@ -0,0 +1,27 @@
+#    _____           ______  _____ 
+#  / ____/    /\    |  ____ |  __ \
+# | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
+# | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
+# | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
+#  \_____\/_/    \_ \______ |_|  \_\
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
+
+
+from ._patches import extract_patches_2d
+
+
+__all__ = [
+    'PatchPreprocess'
+]
+
+
+class PatchPreprocess:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def patch_preprocess(self, image):
+        return extract_patches_2d(image, (self.height, self.width), max_patches=1)[0]
---
+++ t2.py
@@ -0,0 +1,9 @@
+from time import time as t 
+
+s = t()
+import numpy as np 
+import cv2 as cv 
+import caer 
+e = t()
+
+print('T2', e-s)
