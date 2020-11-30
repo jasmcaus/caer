2711ac9e4ea5e6089f7b83e6f16a33e418142d2d
jasmcaus <jasmcaus@gmail.com>
2020-11-30 23:13:02 +0530

    Moved dtype to deprecated

--- caer/utils/__init__.py
+++ caer/utils/__init__.py
@@ -8,23 +8,3 @@
 # Licensed under the MIT License <http://opensource.org/licenses/MIT>
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
-
-
-from .dtype import (
-    img_as_float32, 
-    img_as_float64, 
-    img_as_float,
-    convert_to_float,
-    img_as_int, 
-    img_as_uint, 
-    img_as_ubyte,
-    img_as_bool, 
-    dtype_limits
-)
-
-# __all__ globals 
-from .dtype import __all__ as __all_dtype__
-
-__all__ = __all_dtype__
-
-del __all_dtype__
