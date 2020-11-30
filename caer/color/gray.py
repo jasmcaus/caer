746a361f7ccc1ca8deaf7e9b1aa0d2fc735f0af2
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-30 23:01:17 +0530

    Add typing support

--- caer/color/gray.py
+++ caer/color/gray.py
@@ -11,6 +11,7 @@
 
 
 import cv2 as cv 
+import numpy as np 
 
 from .constants import GRAY2BGR, GRAY2RGB
 from .bgr import bgr_to_lab, bgr_to_hsv
@@ -23,7 +24,7 @@ __all__ = [
 ]
 
 
-def gray_to_rgb(img):
+def gray_to_rgb(img) -> np.ndarray:
     """
         Converts an Grayscale image to its RGB version
     """
@@ -33,7 +34,7 @@ def gray_to_rgb(img):
     return cv.cvtColor(img, GRAY2RGB)
 
 
-def gray_to_bgr(img):
+def gray_to_bgr(img) -> np.ndarray:
     """
         Converts am Grayscale image to its BGR version
     """
@@ -43,26 +44,26 @@ def gray_to_bgr(img):
     return cv.cvtColor(img, GRAY2BGR)
 
 
-def gray_to_lab(img):
+def gray_to_lab(img) -> np.ndarray:
     """
         Converts an Grayscale image to its LAB version
     """
     if len(img.shape) != 3:
         raise ValueError(f'Image of shape 3 expected. Found shape {len(img.shape)}. This method converts a Grayscale image to its LAB counterpart')
 
-    bgr = gray_to_bgr(img)
+    bgr = gray_to_bgr(img) -> np.ndarray
 
     return bgr_to_lab(bgr)
 
 
-def gray_to_hsv(img):
+def gray_to_hsv(img) -> np.ndarray:
     """
         Converts an Grayscale image to its HSV version
     """
     if len(img.shape) != 1:
         raise ValueError(f'Image of shape 3 expected. Found shape {len(img.shape)}. This method converts a LAB image to its HSV counterpart')
 
-    bgr = gray_to_bgr(img)
+    bgr = gray_to_bgr(img) -> np.ndarray
 
     return bgr_to_hsv(bgr)
 
