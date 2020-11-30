e17d111e786d66ecd7edc3d7c02f2bce6d9885fa
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-11-30 22:59:32 +0530

    Remove torch

--- caer/data/__init__.py
+++ caer/data/__init__.py
@@ -13,7 +13,6 @@ from ..io import imread
 from ..path import abspath, minijoin
 from .._base import __curr__ 
 import numpy as np 
-import torch
 
 here = minijoin(__curr__, 'data').replace('\\', "/") + "/"
 
@@ -21,7 +20,7 @@ def get_path_to_data(name) -> str:
     return minijoin(here, name)
 
 
-def audio_mixer(target_size=None, rgb=False) -> torch.Tensor:
+def audio_mixer(target_size=None, rgb=False) -> np.ndarray:
     return imread(here+'audio_mixer.jpg', target_size=target_size, rgb=rgb)
 
 
