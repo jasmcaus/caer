4722057adf8b5639f888f0f8907a8c52757351c3
jasmcaus <jasmcaus@gmail.com>
2020-11-30 18:48:00 +0530

    Remove patch preprocessing

--- caer/preprocessing/__init__.py
+++ caer/preprocessing/__init__.py
@@ -16,13 +16,10 @@ from .mean_subtraction import compute_mean_from_dir
 from .mean_subtraction import subtract_mean
 from .mean_subtraction import _check_mean_sub_values
 
-from .patch_preprocess import PatchPreprocess
 
 # __all__ globals 
 from .mean_subtraction import __all__ as __all_mean__ 
-from .patch_preprocess import __all__ as __all_patch__ 
 
-__all__ = __all_mean__ + __all_patch__
+__all__ = __all_mean__
 
 del __all_mean__
-del __all_patch__
