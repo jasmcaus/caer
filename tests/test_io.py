4f05ad617debd09bf83bc6f47dfc5c0caddfb9e1
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-12-02 15:52:22 +0530

    Update tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -7,7 +7,7 @@ here = os.path.dirname(__file__)
 
 
 def test_imread():
-    test_img = os.path.join(here, 'caer', 'data', 'bear.jpg')
+    test_img = os.path.join(here, 'tests', 'data', 'beverages.jpg')
 
     img = caer.imread(test_img)
     test_against = cv.imread(test_img) 
@@ -16,7 +16,7 @@ def test_imread():
 
 
 def test_gray():
-    test_img = os.path.join(here, 'caer', 'data', 'blue_tang.jpg')
+    test_img = os.path.join(here, 'tests', 'data', 'green_fish.jpg')
 
     img = caer.imread(test_img, channels=1)
 
