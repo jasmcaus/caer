8502cb4d56b4f0a75b97be57edaeec32d2fc031c
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-12-03 10:05:33 +0530

    Update tests

--- tests/test_resize.py
+++ tests/test_resize.py
@@ -11,8 +11,8 @@ def test_target_sizes():
     img_199_206 = caer.imread(test_img, target_size=(199,206))
 
     assert img_400_400.shape[:2] == (400,400)
-    assert img_304_339.shape[:2] == (304,339)
-    assert img_199_206.shape[:2] == (199,206)
+    assert img_304_339.shape[:2] == (339,304)
+    assert img_199_206.shape[:2] == (206,199)
 
 
 def test_keep_aspect_ratio():
@@ -23,5 +23,5 @@ def test_keep_aspect_ratio():
     img_93_35 = caer.imread(test_img, target_size=(93,35), keep_aspect_ratio=True)
 
     assert img_400_400.shape[:2] == (400,400)
-    assert img_223_182.shape[:2] == (223,182)
-    assert img_93_35.shape[:2] == (93,35)
+    assert img_223_182.shape[:2] == (182,223)
+    assert img_93_35.shape[:2] == (35,93)
