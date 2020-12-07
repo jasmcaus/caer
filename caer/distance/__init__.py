e17c6e903beeb7dd2698b0b25b3d03c9de5e8a20
Jason Dsouza <jasmcaus@gmail.com>
2020-12-08 01:20:19 +0530

    Remove c-ext for now

--- caer/distance/__init__.py
+++ caer/distance/__init__.py
@@ -1,16 +1,16 @@
-#    _____           ______  _____ 
-#  / ____/    /\    |  ____ |  __ \
-# | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
-# | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
-# | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
-#  \_____\/_/    \_ \______ |_|  \_\
+# #    _____           ______  _____ 
+# #  / ____/    /\    |  ____ |  __ \
+# # | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
+# # | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
+# # | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
+# #  \_____\/_/    \_ \______ |_|  \_\
 
-# Licensed under the MIT License <http://opensource.org/licenses/MIT>
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
+# # Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# # SPDX-License-Identifier: MIT
+# # Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
 
 
-from .distance import distance 
-from .distance import __all__ as __all_dist__
+# from .distance import distance 
+# from .distance import __all__ as __all_dist__
  
-__all__ = __all_dist__
+# __all__ = __all_dist__
