4e6d69264816eaf18e22ed95a628c798d2b4d698
Jason Dsouza <jasmcaus@gmail.com>
2020-12-08 13:42:41 +0530

    Add transforms

--- caer/transforms/__init__.py
+++ caer/transforms/__init__.py
@@ -8,3 +8,16 @@
 # Licensed under the MIT License <http://opensource.org/licenses/MIT>
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
+
+
+from .transform import (
+    hflip,
+    vflip,
+    hvflip,
+    __all__ as __all_trans__
+)
+
+__all__ = __all_trans__ 
+
+# Stop polluting the namespace
+del __all_trans__
