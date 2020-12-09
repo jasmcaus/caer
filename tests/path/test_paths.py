38a349ac7ba344fe851a78ca5d821ff82321d212
Jason Dsouza <jasmcaus@gmail.com>
2020-12-09 14:33:22 +0530

    Add tests for path

--- tests/path/test_paths.py
+++ tests/path/test_paths.py
@@ -10,7 +10,93 @@
 # Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
 
 import os 
-import caer 
+import caer
+
+
+PATH_TO_MEDIA_FILES = os.path.join(os.path.dirname(os.getcwd()), 'media-files')
+
+
+def test_list_images():
+    DIR = PATH_TO_MEDIA_FILES
+
+    images_list = caer.path.list_images(DIR, include_subdirs=True, use_fullpath=True)
+
+    assert images_list is not None 
+    assert len(images_list) == 18 #There are 18 images & 7 videos in tests/media-files
+
+    for i in images_list:
+        assert os.path.exists(i) 
+
+
+def test_list_videos():
+    DIR = PATH_TO_MEDIA_FILES
+
+    videos_list = caer.path.list_videos(DIR, include_subdirs=True, use_fullpath=True)
+
+    assert videos_list is not None 
+    assert len(videos_list) == 7 #There are 18 images & 7 videos in tests/media-files
+
+    for i in videos_list:
+        assert os.path.exists(i) 
+
+
+def test_list_media():
+    DIR = PATH_TO_MEDIA_FILES
+
+    media_list = caer.path.list_media(DIR, include_subdirs=True, use_fullpath=True)
+
+    assert media_list is not None 
+    assert len(media_list) == 25 #There are 18 images & 7 videos in tests/media-files
+
+    for i in media_list:
+        assert os.path.exists(i) 
+
+
+def test_listdir():
+    DIR = PATH_TO_MEDIA_FILES
+
+    dir_list = caer.path.listdir(DIR, include_subdirs=True, use_fullpath=True)
+
+    assert dir_list is not None 
+    assert len(dir_list) == 19 #There are 18 images, 7 videos and 1 README.md in tests/media-files
+
+    for i in dir_list:
+        assert os.path.exists(i) 
+
+
+def test_is_image():
+    IMAGE = os.path.join(PATH_TO_MEDIA_FILES, 'birger-strahl-CkZhVAV5ZNE-unsplash.jpg')
+
+    assert caer.path.is_image(IMAGE)
+
+
+def test_is_video():
+    VIDEO = os.path.join(PATH_TO_MEDIA_FILES, 'Keyboard - 1046.mp4')
+
+    assert caer.path.is_video(VIDEO)
+
+
+def test_isfile():
+    FILE = os.path.join(PATH_TO_MEDIA_FILES, 'birger-strahl-CkZhVAV5ZNE-unsplash.jpg')
+
+    assert caer.path.isfile(FILE) == os.path.isfile(FILE)
+
+
+def test_isdir():
+    DIR = PATH_TO_MEDIA_FILES
+
+    assert caer.path.isdir(DIR) == os.path.isdir(DIR)
+
+
+def test_get_size():
+    FILE = os.path.join(PATH_TO_MEDIA_FILES, 'GoPro', 'gopro3')
+
+    assert caer.path.get_size(FILE, 'bytes') == os.path.getsize(FILE)
+    assert caer.path.get_size(FILE, 'kb') == (os.path.getsize(FILE) * 1e-3)
+    assert caer.path.get_size(FILE, 'mb') == (os.path.getsize(FILE) * 1e-6)
+    assert caer.path.get_size(FILE, 'gb') == (os.path.getsize(FILE) * 1e-9)
+    assert caer.path.get_size(FILE, 'tb') == (os.path.getsize(FILE) * 1e-12)
+
 
 def test_osname():
     s = caer.path.osname()
