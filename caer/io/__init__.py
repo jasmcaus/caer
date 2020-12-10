d8842b3682cbc00e890508cf902c644625401c4d
Jason Dsouza <jasmcaus@gmail.com>
2020-12-10 12:50:09 +0530

    Add io

---
+++ caer/io/__init__.py
@@ -0,0 +1,26 @@
+#    _____           ______  _____ 
+#  / ____/    /\    |  ____ |  __ \
+# | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
+# | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
+# | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
+#  \_____\/_/    \_ \______ |_|  \_\
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
+
+# Resize
+from .resize import (
+    resize,
+    smart_resize,
+    __all__ as __all_res__
+)
+
+# Image 
+from .io import (
+    imread,
+    imsave,
+    __all__ as __all_io__
+)
+
+__all__ = __all_io__ + __all_res__
