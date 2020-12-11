84ba7bf22317b73cf8c683bc37bd88407b2c5de8
Jason Dsouza <jasmcaus@gmail.com>
2020-12-11 20:13:38 +0530

    Update tests

--- caer/io/io.py
+++ caer/io/io.py
@@ -12,6 +12,7 @@
 
 import cv2 as cv
 from urllib.request import urlopen
+import warnings
 
 from ..core import asarray
 from ..color import bgr_to_rgb, bgr_to_gray, rgb_to_bgr, IMREAD_COLOR
@@ -26,22 +27,22 @@ __all__ = [
 ]
 
 
-def imread(image_path, target_size=None, channels=3, rgb=True, resize_factor=None, preserve_aspect_ratio=False, interpolation='bilinear'):
+def imread(image_path, target_size=None, rgb=True, gray=False, resize_factor=None, preserve_aspect_ratio=False, interpolation='bilinear', channels=None):
     r"""
         Loads in an image from `image_path` (can be either a system filepath or a URL)
 
         Args:
             image_path (str): Filepath/URL to read the image from.
             target_size (tuple): Target size. Must be a tuple of ``(width, height)`` integer.
-            channels: 1 (grayscale) or 3 (RGB/BGR). Default: 3
-            rgb: Boolean to keep RGB ordering. Default: True
-            resize_factor (float, tuple): Resizing Factor to employ. 
-                Shrinks the image if ``resize_factor < 1``
-                Enlarges the image if ``resize_factor > 1``
+            rgb (bool): Boolean to keep RGB ordering. Default: True
+            gray (bool): Boolean to convert to Grayscale. Default: False
+            resize_factor (float, tuple): Resizing Factor to employ.   
+                Shrinks the image if ``resize_factor < 1``.
+                Enlarges the image if ``resize_factor > 1``.
             preserve_aspect_ratio (bool): Prevent aspect ratio distortion (employs center crop).
             interpolation (str): Interpolation to use for resizing. Defaults to `'bilinear'`. 
                 Supports `'bilinear'`, `'bicubic'`, `'area'`, `'nearest'`.
-
+            channels (int): :warning: Deprecated. Use ``gray`` instead.
 
         Returns:
             Array with shape ``(height, width, channels)``.
@@ -58,7 +59,130 @@ def imread(image_path, target_size=None, channels=3, rgb=True, resize_factor=Non
             (427, 640, 3)
 
     """
-    return _imread(image_path, target_size=target_size, channels=channels, rgb=rgb, resize_factor=resize_factor, preserve_aspect_ratio=preserve_aspect_ratio, interpolation=interpolation)
+
+    if channels is not None:
+        warnings.warn('`channels` is deprecated. Use `gray=True` instead for grayscale images. See the API documentation for more details.')
+        
+    # Don't use an else here. 
+    # For some reason `warnings` blocks the execution of an else statement
+    if channels is None:
+        # Checks if `rgb` and `gray` are bools
+        channels = _return_num_channels(rgb=rgb, gray=gray)
+        
+    if channels == 1:
+        # Preference goes to Grayscale
+        rgb = False 
+    
+
+    return _imread(image_path, rgb=rgb, channels=channels, target_size=target_size, resize_factor=resize_factor, preserve_aspect_ratio=preserve_aspect_ratio, interpolation=interpolation)
+
+
+def _imread(image_path, rgb=True, channels=3, target_size=None, resize_factor=None, preserve_aspect_ratio=False, interpolation='bilinear'):   
+    if target_size is not None:
+        _ = _check_target_size(target_size)
+        
+    # if not isinstance(channels, int) or channels not in [1, 3]:
+    #     raise ValueError('channels must be an integer - 1 (Grayscale) or 3 (RGB)')
+
+    interpolation_methods = {
+        'nearest':  0,  '0': 0, 
+        'bilinear': 1,  '1': 1,
+        'bicubic':  2,  '2': 2,
+        'area':     3,  '3': 3,
+    }
+
+    if interpolation not in interpolation_methods:
+        raise ValueError('Specify a valid interpolation type - area/nearest/bicubic/bilinear')
+
+    try:
+        # If channels=1, we convert it to grayscale later on
+        image_array = _url_to_image(image_path, rgb=True)
+    except Exception:
+        if exists(image_path):
+            image_array = _read_image(image_path)
+        else:
+            raise ValueError('Specify either a valid URL or valid filepath')
+            
+
+    # [INFO] Using the following piece of code results in a 'None'
+    # if image_array == None:
+    #     pass
+    
+    if channels == 1:
+        # TBH, rgb_to_gray() and bgr_to_gray() is pretty much the same thing
+        image_array = bgr_to_gray(image_array)
+
+    if target_size is not None or resize_factor is not None:
+        image_array = resize(image_array, target_size, resize_factor=resize_factor, preserve_aspect_ratio=preserve_aspect_ratio, interpolation=interpolation)
+
+    if rgb:
+        image_array = bgr_to_rgb(image_array)
+
+    return image_array
+
+
+def _read_image(image_path):
+    """Reads an image as a BGR image located at `path`.
+
+    Args:
+        path (str): Path to a valid image file in the filesystem.
+
+    Returns:
+        Array of size `(height, width, channels)`.
+    """
+    if not exists(image_path):
+        raise FileNotFoundError('The image file was not found')
+    
+    return cv.imread(image_path)
+
+
+def _url_to_image(url, rgb=True):
+    # Converts the image to a Numpy array and reads it in OpenCV
+    response = urlopen(url)
+    image = asarray(bytearray(response.read()), dtype='uint8')
+    image = cv.imdecode(image, IMREAD_COLOR)
+
+    if image:
+        if rgb:
+            image = bgr_to_rgb(image)
+        return image
+    else:
+        raise ValueError(f'No such image at {url}')
+
+
+def _return_num_channels(rgb, gray):
+    """
+        ``rgb`` and ``gray`` must both be boolean values. 
+        Returns number of channels (1 or 3) based on the above values.
+        
+        Consider the table below with keys 
+        rgb     |    gray    |   returned value (no. of channels)
+        --------|------------|-------------------------------------
+        True    |    False   |   3   # Returns RGB Image
+        True    |    True    |   1   # Returns Grayscale Image
+        False   |    True    |   3   # Returns Grayscale Image
+        False   |    False   |   3   # Returns BGR Image
+    """
+
+    # Handling the edge cases
+    if not isinstance(rgb, bool):
+        raise ValueError('`rgb` must be a boolean value')
+    
+    if not isinstance(gray, bool):
+        raise ValueError('`gray` must be a boolean value')
+
+    # We use this approach:
+    #   Preference is given to gray. 
+    #   If gray == True, then we return channels=1
+    
+    # rgb can be either True or False 
+    # IF rgb == True, it implies that the user expects an RGB image
+    # If rgb == False, we return a BGR image Tensor 
+
+    if gray:
+        return 1 # Grayscale images are 1-channeled
+    else:
+        return 3 # BGR/RGB images are 3-channeled
 
 
 def imsave(path, img, rgb=True):
@@ -87,81 +211,3 @@ def imsave(path, img, rgb=True):
         return cv.imwrite(path, img)
     except:
         raise ValueError('`img` needs to be an opencv-specific image. Try reading the image using `caer.imread()`. More support for additional platforms will follow. Check the Changelog for further details.')
-
-
-def _imread(image_path, target_size=None, channels=3, rgb=True, resize_factor=None, preserve_aspect_ratio=False, interpolation='bilinear'):   
-    if target_size is not None:
-        _ = _check_target_size(target_size)
-        
-    if not isinstance(channels, int) or channels not in [1,3]:
-        raise ValueError('channels must be an integer - 1 (Grayscale) or 3 (RGB)')
-
-    if not isinstance(rgb, bool):
-        raise ValueError('rgb must be a boolean')
-    
-    if rgb and channels == 1:
-        # Preference goes to Grayscale
-        rgb = False
-    
-    interpolation_methods = {
-        'nearest': 0,  '0': 0, 
-        'bilinear': 1, '1': 1,
-        'bicubic': 2,  '2': 2,
-        'area': 3,     '3': 3,
-    }
-
-    if interpolation not in interpolation_methods:
-        raise ValueError('Specify a valid interpolation type - area/nearest/bicubic/bilinear')
-
-    try:
-        image_array = _url_to_image(image_path, rgb=True)
-    except Exception:
-        if exists(image_path):
-            image_array = _read_image(image_path)
-        else:
-            raise ValueError('Specify either a valid URL or valid filepath')
-            
-
-    # [INFO] Using the following piece of code results in a 'None'
-    # if image_array == None:
-    #     pass
-    
-    if channels == 1:
-        image_array = bgr_to_gray(image_array)
-
-    if target_size is not None or resize_factor is not None:
-        image_array = resize(image_array, target_size, resize_factor=resize_factor, preserve_aspect_ratio=preserve_aspect_ratio, interpolation=interpolation)
-
-    if rgb:
-        image_array = bgr_to_rgb(image_array)
-
-    return image_array
-
-
-def _read_image(image_path):
-    """Reads an image located at `path` into an array.
-
-    Args:
-        path (str): Path to a valid image file in the filesystem.
-
-    Returns:
-        Array of size `(height, width, channels)`.
-    """
-    if not exists(image_path):
-        raise FileNotFoundError('The image file was not found')
-    
-    return cv.imread(image_path)
-
-
-def _url_to_image(url, rgb=True):
-    # Converts the image to a Numpy array and reads it in OpenCV
-    response = urlopen(url)
-    image = asarray(bytearray(response.read()), dtype='uint8')
-    image = cv.imdecode(image, IMREAD_COLOR)
-
-    if image:
-        if rgb:
-            image = bgr_to_rgb(image)
-        return image
-    else:
-        raise ValueError(f'No such image at {url}')
--- tests/io/test_io.py
+++ tests/io/test_io.py
@@ -19,17 +19,63 @@ here = os.path.dirname(os.path.dirname(__file__))
 
 
 def test_imread():
-    test_img = os.path.join(here, 'data', 'beverages.jpg')
+    img_path = os.path.join(here, 'data', 'beverages.jpg')
 
-    img = caer.imread(test_img)
-    test_against = cv.imread(test_img) 
+    cv_bgr = cv.imread(img_path) 
+    cv_rgb = cv.cvtColor(cv_bgr.copy(), cv.COLOR_BGR2RGB)
+    cv_gray = cv.cvtColor(cv_bgr.copy(), cv.COLOR_BGR2GRAY)
+    
 
-    assert np.all(img == test_against)
+    caer_bgr = caer.imread(img_path, rgb=False) # Expected: BGR
+    caer_rgb = caer.imread(img_path, rgb=True) # Expected: RGB
+
+    caer_rgbF_grayF = caer.imread(img_path, rgb=False, gray=False) # Expected: BGR
+    caer_rgbT_grayF = caer.imread(img_path, rgb=True, gray=False) # Expected: RGB
+    caer_rgbT_grayT = caer.imread(img_path, rgb=True, gray=True) # Expected: Gray
+    caer_rgbF_grayT = caer.imread(img_path, rgb=False, gray=True) # Expected: Gray
+    caer_rgbT_grayT = caer.imread(img_path, rgb=True, gray=True) # Expected: Gray
+
+
+    # Asserts
+    assert np.all(caer_bgr == cv_bgr)
+    assert np.all(caer_rgb == cv_rgb)
+
+    assert np.all(caer_rgbF_grayF == cv_bgr)
+    assert np.all(caer_rgbT_grayF == cv_rgb)
+    assert np.all(caer_rgbT_grayT == cv_gray)
+    assert np.all(caer_rgbF_grayT == cv_gray)
+    assert np.all(caer_rgbT_grayT == cv_gray)
+
+
+def test_imread_target_sizes():
+    img_path = os.path.join(here, 'data', 'beverages.jpg')
+
+    img_400_400 = caer.imread(img_path, target_size=(400,400))
+    img_304_339 = caer.imread(img_path, target_size=(304,339))
+    img_199_206 = caer.imread(img_path, target_size=(199,206))
+
+    assert img_400_400.shape[:2] == (400,400)
+    assert img_304_339.shape[:2] == (339,304) # Numpy arrays are processed differently (h,w) as opposed to (w,h)
+    assert img_199_206.shape[:2] == (206,199) # Numpy arrays are processed differently (h,w) as opposed to (w,h)
+
+
+def test_imread_preserve_aspect_ratio():
+    img_path = os.path.join(here, 'data', 'green_fish.jpg')
+
+    img_400_400 = caer.imread(img_path, target_size=(400,400), preserve_aspect_ratio=True)
+    img_223_182 = caer.imread(img_path, target_size=(223,182), preserve_aspect_ratio=True)
+    img_93_35 = caer.imread(img_path, target_size=(93,35), preserve_aspect_ratio=True)
+
+    assert img_400_400.shape[:2] == (400,400)
+    assert img_223_182.shape[:2] == (182,223) # Numpy arrays are processed differently (h,w) as opposed to (w,h)
+    assert img_93_35.shape[:2] == (35,93) # Numpy arrays are processed differently (h,w) as opposed to (w,h)
 
 
 def test_gray():
-    test_img = os.path.join(here, 'data', 'green_fish.jpg')
+    img_path = os.path.join(here, 'data', 'green_fish.jpg')
 
-    img = caer.imread(test_img, channels=1)
+    img = caer.imread(img_path, gray=True)
+    img_chann = caer.imread(img_path, channels=1) # Maintain backwards-compatibility
 
     assert len(img.shape) == 2
+    assert len(img_chann.shape) == 2
