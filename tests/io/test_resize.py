c2525dc41a59bef41236abf4a855d31c1f82c863
jasmcaus <jasmcaus@gmail.com>
2020-12-11 20:13:38 +0530

    Update tests

--- tests/io/test_resize.py
+++ tests/io/test_resize.py
@@ -16,24 +16,28 @@ import os
 here = os.path.dirname(os.path.dirname(__file__))
 
 def test_target_sizes():
-    test_img = os.path.join(here, 'data', 'beverages.jpg')
+    img_path = os.path.join(here, 'data', 'beverages.jpg')
 
-    img_400_400 = caer.imread(test_img, target_size=(400,400))
-    img_304_339 = caer.imread(test_img, target_size=(304,339))
-    img_199_206 = caer.imread(test_img, target_size=(199,206))
+    img = caer.imread(img_path)
+
+    img_400_400 = caer.resize(img, target_size=(400,400))
+    img_304_339 = caer.resize(img, target_size=(304,339))
+    img_199_206 = caer.resize(img, target_size=(199,206))
 
     assert img_400_400.shape[:2] == (400,400)
-    assert img_304_339.shape[:2] == (339,304) # Numpy arrays are processes differently (h,w) as opposed to (w,h)
-    assert img_199_206.shape[:2] == (206,199) # Numpy arrays are processes differently (h,w) as opposed to (w,h)
+    assert img_304_339.shape[:2] == (339,304) # Numpy arrays are processed differently (h,w) as opposed to (w,h)
+    assert img_199_206.shape[:2] == (206,199) # Numpy arrays are processed differently (h,w) as opposed to (w,h)
 
 
 def test_preserve_aspect_ratio():
-    test_img = os.path.join(here, 'data', 'green_fish.jpg')
+    img_path = os.path.join(here, 'data', 'green_fish.jpg')
 
-    img_400_400 = caer.imread(test_img, target_size=(400,400), preserve_aspect_ratio=True)
-    img_223_182 = caer.imread(test_img, target_size=(223,182), preserve_aspect_ratio=True)
-    img_93_35 = caer.imread(test_img, target_size=(93,35), preserve_aspect_ratio=True)
+    img = caer.imread(img_path)
+
+    img_400_400 = caer.resize(img, target_size=(400,400), preserve_aspect_ratio=True)
+    img_223_182 = caer.resize(img, target_size=(223,182), preserve_aspect_ratio=True)
+    img_93_35 = caer.resize(img, target_size=(93,35), preserve_aspect_ratio=True)
 
     assert img_400_400.shape[:2] == (400,400)
-    assert img_223_182.shape[:2] == (182,223) # Numpy arrays are processes differently (h,w) as opposed to (w,h)
-    assert img_93_35.shape[:2] == (35,93) # Numpy arrays are processes differently (h,w) as opposed to (w,h)
+    assert img_223_182.shape[:2] == (182,223) # Numpy arrays are processed differently (h,w) as opposed to (w,h)
+    assert img_93_35.shape[:2] == (35,93) # Numpy arrays are processed differently (h,w) as opposed to (w,h)
