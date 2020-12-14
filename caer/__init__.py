9c701632eee21d57e8f3d119216888c0d9a8a16a
Jason Dsouza <jasmcaus@gmail.com>
2020-12-14 23:08:38 +0530

    transforms ==> augment

--- caer/__init__.py
+++ caer/__init__.py
@@ -1,13 +1,18 @@
-""" \
+r""" \
 Caer
 =====
-A Computer Vision library in Python with powerful image and video processing operations.
-Caer is a set of utility functions designed to help speed up your Computer Vision workflow. Functions inside `caer` will help reduce the number of calculation calls your code makes, ultimately making it neat, concise and readable.
+Caer is a *lightweight, scalable* Computer Vision library for high-performance AI research. It simplifies your approach towards Computer Vision by abstracting away unnecessary boilerplate code giving you the flexibility to quickly prototype deep learning models or research ideas. 
+
+Our design philosophy makes Caer ideal for students, researchers, hobbyists and even experts in the fields of Deep Learning and Computer Vision.
 
 Documentation: https://caer.rtfd.io
 
 Available subpackages
 ---------------------
+augment
+    Powerful image augmentation operations.
+color
+    Colorspace manipulation
 data
     Standard high-quality test images
 filters
@@ -16,8 +21,6 @@ path
     OS-specific Path Manipulations
 preprocessing
     Image preprocessing utilities
-utils
-    General utilities
 video
     Video processing utilities
 
@@ -26,6 +29,15 @@ Utilities
 ---------
 __version__
     Caer version string
+
+__author__
+    Author of Caer
+
+__contributors__
+    List of all contributors to the project
+
+__homepage__
+    Web URL of the Caer documentation
 
 """
 
@@ -40,7 +52,7 @@ from ._meta import (
     homepage
 )
 
-"""
+r"""
 Root Package Info
 """
 
@@ -50,7 +62,7 @@ __author__ = author
 __author_email__ = author_email
 __contributors__ = contributors
 __license__ = 'MIT License'
-__copyright__ = """
+__copyright__ = r"""
 Copyright (c) 2020 Jason Dsouza <jasmcaus>
 All Rights Reserved.
 """
@@ -145,25 +157,24 @@ def get_caer_methods():
 # __all__ configs
 from .globals import __all__ as __all_globals__
 
-from .video import __all__ as __all_video__
-from .preprocessing import __all__ as __all_preprocessing__
-from .data import __all__ as __all_data__
-from .utils import __all__ as __all_utils__
-from .path import __all__ as __all_path__
-from .filters import __all__ as __all_filters__
 from .augment import __all__ as __all_augment__
+from .data import __all__ as __all_data__
+from .filters import __all__ as __all_filters__
+from .path import __all__ as __all_path__
+from .preprocessing import __all__ as __all_preprocessing__
+from .video import __all__ as __all_video__
 
 
-__all__ = __all_globals__ + __all_core__ + __all_preprocess__ + __all_filters__
+__all__ = __all_globals__ + __all_core__ + __all_preprocess__
 
+__all__ += __all_augment__
+__all__ += __all_color__
+__all__ += __all_data__ 
+__all__ += __all_filters__ 
 __all__ += __all_io__ 
+__all__ += __all_path__
 __all__ += __all_preprocessing__ 
 __all__ += __all_video__ 
-__all__ += __all_data__ 
-__all__ += __all_utils__ 
-__all__ += __all_path__
-__all__ += __all_color__
-__all__ += __all_augment__
 
 
 # Stop polluting the namespace
@@ -178,12 +189,10 @@ del contributors
 del __all_globals__ 
 del __all_core__ 
 del __all_preprocess__ 
-del __all_visualizations__
 
 del __all_augment__ 
 del __all_color__ 
 del __all_data__ 
 del __all_filters__
 del __all_preprocessing__ 
-del __all_utils__ 
 del __all_video__ 
