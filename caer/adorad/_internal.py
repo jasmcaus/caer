5505bf657f8d86187a55f41b3225bdc511c6f0b1
jasmcaus <jasmcaus@gmail.com>
2021-01-12 14:53:23 +0530

    Add internal script

---
+++ caer/adorad/_internal.py
@@ -0,0 +1,15 @@
+#    _____           ______  _____ 
+#  / ____/    /\    |  ____ |  __ \
+# | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
+# | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
+# | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
+#  \_____\/_/    \_ \______ |_|  \_\
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 The Caer Authors <http://github.com/jasmcaus>
+
+import numpy as np 
+
+def cat(seq, axis=0, out=None):
+    return np.concatenate((s for s in seq), axis=axis, out=out)
