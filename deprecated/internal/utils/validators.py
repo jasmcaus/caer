96c429ab41ea1c81a7ada8ad961196e649ccea42
Mike A <jimalik2525@gmail.com>
2021-01-20 11:58:00 -0500

    Updating Copyright, adjusting 2021 to 2020-2021 as per owner request

--- deprecated/internal/utils/validators.py
+++ deprecated/internal/utils/validators.py
@@ -7,7 +7,7 @@
 
 # Licensed under the MIT License <http://opensource.org/licenses/MIT>
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2021 The Caer Authors <http://github.com/jasmcaus>
+# Copyright (c) 2020-2021 The Caer Authors <http://github.com/jasmcaus>
 
 
 import ipaddress
