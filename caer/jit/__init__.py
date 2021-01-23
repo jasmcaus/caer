5cce81337c24684803b1c5edc54a7011194b6ac9
Jason Dsouza <jasmcaus@gmail.com>
2021-01-23 10:20:19 +0530

    Add license info to `jit`

---
+++ caer/jit/__init__.py
@@ -0,0 +1,10 @@
+#    _____           ______  _____ 
+#  / ____/    /\    |  ____ |  __ \
+# | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
+# | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
+# | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
+#  \_____\/_/    \_ \______ |_|  \_\
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020-21 The Caer Authors <http://github.com/jasmcaus>
--- caer/jit/_jit_internal.py
+++ caer/jit/_jit_internal.py
@@ -1,6 +1,17 @@
-#pylint:disable=unused-import 
+#    _____           ______  _____ 
+#  / ____/    /\    |  ____ |  __ \
+# | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
+# | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++
+# | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
+#  \_____\/_/    \_ \______ |_|  \_\
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020-21 The Caer Authors <http://github.com/jasmcaus>
+
+# #pylint:disable=unused-import 
 
 from typing import (
     Any, Tuple, BinaryIO, Callable, ContextManager, Dict, Iterator, List, NamedTuple,
-    Optional, overload, Sequence, Tuple, TypeVar, Type, Union, Generic, Set, AnyStr
+    Optional, overload, Sequence, TypeVar, Type, Union, Generic, Set, AnyStr
 )
