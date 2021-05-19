c76e83db0be2a838a6a2700c1d7298098d764e77
jasmcaus <jasmcaus@gmail.com>
2021-05-20 01:25:40 +0530

    Add README

--- examples/GUI/caer_gui.py
+++ examples/GUI/caer_gui.py
@@ -1,24 +1,27 @@
-# Simple tkinter GUI app example, designed to showcase some caer features
-# Should only be used as a base to create a new GUI
-# It can be re-designed, controls re-grouped and code improved
+# A simple TkInter GUI app example, designed to showcase some of Caer's features
+# This should only be used as a base to create a new GUI.
 
-# Requirements: python3, caer, matplotlib
+# Requirements: Python, Caer, Matplotlib
 
 # Run it either via IDLE or from command prompt / terminal with one of these commands:
-# - 'python caer_gui_test.py'
-# - 'python -m caer_gui_test'
-# - 'python3 caer_gui_test.py'
-# - 'python3 -m caer_gui_test'
+# - 'python caer_gui.py'
+# - 'python -m caer_gui'
+# - 'python3 caer_gui.py'
+# - 'python3 -m caer_gui'
 
-# Tested as working in Windows 10 with python v3.6.8 and Kubuntu Linux with python v3.6.8
-# You can select one of 9 built-in images to display (startup has "Island" selected as default)
-# Selecting any of the images, at any point in time, will always start with a fresh original image and reset controls.
+# Tested as working in Windows 10 with python v3.6.8 and Kubuntu Linux with python v3.6.8. 
+# You can select one of 9 built-in images to display (startup has `caer.data.sland` selected as default)
+# Selecting any of the images, at any point in time, will always start with a fresh original image and reset 
+# controls.
 # Replace with or add your own image(s) by following the instructions here: https://caer.readthedocs.io/en/latest/api/io.html
-# The above will require that you modify main() and show_original_image() functions
-# All function controls are set to manipulate the currently displayed image
-# Edges and Emboss effects are mutually exclusive (you can only have one applied at the time)
-# Gamma, Hue, Saturation, Sharpness, Posterize, Solarize, Edges and Emboss effects are currently somewhat unique and, when applied to the image, will follow the following rule:
-# - Applying 'Resize', 'Rotate' and/or any of the 'Flip' functions to transformed image will preserve that image and have all those effects reset
+# The above will require that you modify main() and show_original_image() functions.
+# All function controls are set to manipulate the currently displayed image.
+# Edges and Emboss effects are mutually exclusive (you can only have one applied at the time).
+# Gamma, Hue, Saturation, Sharpness, Posterize, Solarize, Edges and Emboss effects are currently somewhat unique 
+# and, when applied to the image, will follow the following rule:
+#   - Applying 'Resize', 'Rotate' and/or any of the 'Flip' functions to transformed image will preserve that image 
+#     and have all those effects reset
+#
 # The above mentioned could possibly be corrected by converting all those buttons to checkboxes and applying all the effects within a single function (just use the current adjust_ghsps() function)
 # The 'Rotation' button is currently set to keep on rotating the image with every tap
 
