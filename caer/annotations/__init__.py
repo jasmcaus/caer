bd813753fafc3ece3e6b0e0ebf2dc7704648b69b
jasmcaus <jasmcaus@gmail.com>
2021-10-06 12:54:07 +0530

    Add `annotations`

---
+++ caer/annotations/__init__.py
@@ -0,0 +1,15 @@
+#    _____           ______  _____ 
+#  / ____/    /\    |  ____ |  __ \
+# | |        /  \   | |__   | |__) | Caer - Modern Computer Vision
+# | |       / /\ \  |  __|  |  _  /  Languages: Python, C, C++, Cuda
+# | |___   / ____ \ | |____ | | \ \  http://github.com/jasmcaus/caer
+#  \_____\/_/    \_ \______ |_|  \_\
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020-2021 The Caer Authors <http://github.com/jasmcaus>
+
+from typing import (
+    Any, Tuple, BinaryIO, Callable, ContextManager, Dict, Iterator, List, NamedTuple,
+    Optional, overload, Sequence, TypeVar, Type, Union, Generic, Set, AnyStr
+)
